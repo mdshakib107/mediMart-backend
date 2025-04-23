d8d67127fe616fa83be35cbe54ff4426e6a0ac16
MarGoN <monondcosta@gmail.com>
2025-04-23 10:24:31 +0600

    Fix: fixed app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,12 @@ import express, { Application, Request, Response } from 'express';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
 import router from './app/routes';
+import HttpStatus from 'http-status';
 
 const app: Application = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: [
     'http://localhost:5173',
     '*'
@@ -35,6 +37,16 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.use(globalErrorHandler);
+
+
+// route not found
+app.use('*', (req: Request, res: Response) => {
+    res.status(HttpStatus.FORBIDDEN).send({
+      success: false,
+      message: 'Route not found!',
+      status: HttpStatus.FORBIDDEN,
+    });
+  });
 
 app.use(notFound as any);
 
