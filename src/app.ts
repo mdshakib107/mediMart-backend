f674eb997bfa64185b8d8f65d9785bade5654966
MarGoN <monondcosta@gmail.com>
2025-04-25 21:23:32 +0600

    Fix: tried to fix cors related problem

--- src/app.ts
+++ src/app.ts
@@ -9,32 +9,32 @@ import notFound from './app/middlewares/notFound';
 import router from './app/routes';
 
 const app: Application = express();
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
-// app.use(
-//   cors({
-//     origin: ['http://localhost:3000'],
-//     credentials: true,
-//   })
-// );
-const allowedOrigins = [
-  'http://localhost:3000',
-  //'https://your-frontend-deployed-url.vercel.app', // if deployed
-];
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: ['http://localhost:3000'],
     credentials: true,
   })
 );
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+// const allowedOrigins = [
+//   'http://localhost:3000',
+//   //'https://your-frontend-deployed-url.vercel.app', // if deployed
+// ];
+
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error('Not allowed by CORS'));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
 
 app.use('/api', router);
 
